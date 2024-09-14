36f4ccea310d373c928e51bf4c57bbaa03c0f030
Desiree Ann Guzman <desireeann000@gmail.com>
2024-09-15 05:04:11 +0800

    commit with dataConversion.js

---
+++ dataConversion.js
@@ -0,0 +1,29 @@
+// CONVERT STRING TO A NUMBER
+let price = "200";
+// parseInt()
+let a = parseInt(price);
+console.log("Using parseInt():", a);
+// Number()
+let b = Number(price);
+console.log("Using Number():", b);
+// + Operator
+let c = +price;
+console.log("Using + operator:", c);
+
+
+// CONVERT NUMBER TO A STRING
+let distance = 150;
+// .toString()
+let y = distance.toString();
+console.log("Using toString():", y);
+
+// String()
+let x = String(distance);
+console.log("Using String():", x);
+
+
+// CONVERT STRING TO A DECIMAL NUMBER
+let weight = "72.5";
+// parseFloat()
+let z = parseFloat(weight);
+console.log("Using parseFloat():", z);
